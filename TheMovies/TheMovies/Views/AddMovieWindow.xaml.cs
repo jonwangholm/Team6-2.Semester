2d1d9a6ba54f09755de59969cb266941ac52c648
SeymenNW <seymennw@gmail.com>
2023-02-03 11:56:33 +0100

    fixed but - en anden metode til at gøre det på.

--- TheMovies/TheMovies/Views/AddMovieWindow.xaml.cs
+++ TheMovies/TheMovies/Views/AddMovieWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using TheMovies.Model;
 using TheMovies.ViewModel;
 
 namespace TheMovies.View
@@ -22,6 +23,7 @@ namespace TheMovies.View
 
     public partial class AddMovieWindow : Window
     {
+        public Film MyProperty { get; set; }
         MainViewModel mvm = new MainViewModel();
 
 
