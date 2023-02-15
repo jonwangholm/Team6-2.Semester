658fe377a01638413329ab435986443b7fd8304a
monaz19 <monaz19@student.sdu.dk>
2023-02-15 12:29:23 +0100

    delete finish

--- TheMovies/TheMovies/Commands/DeleteMovieCommand.cs
+++ TheMovies/TheMovies/Commands/DeleteMovieCommand.cs
@@ -12,10 +12,27 @@ namespace TheMovies.Commands
 {
     public class DeleteMovieCommand : ICommand
     {
-        public event EventHandler? CanExecuteChanged;
+        public event EventHandler? CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested+= value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
 
         public bool CanExecute(object? parameter)
         {
+            if (parameter is MainViewModel mvm)
+            {
+                if (mvm.S1 == null)
+                {
+                    return false;
+                }
+            }
             return true;
         }
 
