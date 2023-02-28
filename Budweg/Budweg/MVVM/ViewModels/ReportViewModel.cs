30c2106a9fe55c152b7a87aa78a8e14094c60013
SeymenNW <seymennw@gmail.com>
2023-02-28 13:25:27 +0100

    some classes

---
+++ Budweg/Budweg/MVVM/ViewModels/ReportViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Budweg.MVVM.ViewModels
+{
+    public class ReportViewModel
+    {
+    }
+}
