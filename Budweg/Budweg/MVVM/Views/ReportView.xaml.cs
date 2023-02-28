e05dd6662f23b8b45a8bb93ee9f9cf27ca3e6103
SeymenNW <seymennw@gmail.com>
2023-02-28 14:54:20 +0100

    GUI Related stuff

--- Budweg/Budweg/MVVM/Views/ReportView.xaml.cs
+++ Budweg/Budweg/MVVM/Views/ReportView.xaml.cs
@@ -24,5 +24,10 @@ namespace Budweg.MVVM.Views
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
