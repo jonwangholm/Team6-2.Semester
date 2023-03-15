7952cdd13541e8411f36143b035993af47552227
monaz19 <monaz19@student.sdu.dk>
2023-03-15 11:26:33 +0100

    Unittest Delete method

--- Budweg/BudwegTest/UnitTest1.cs
+++ Budweg/BudwegTest/UnitTest1.cs
@@ -1,13 +1,14 @@
 using Budweg;
 using Budweg.MVVM.ViewModels.Persistence;
 using Budweg.MVVM.Models;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities.Serialization;
 
 namespace BudwegTest
 {
     [TestClass]
     public class UnitTest1
     {
-        Employee e1;
+        Employee e1,e2;
         EmployeeRepository er1;
 
         [TestInitialize]
@@ -15,6 +16,7 @@ namespace BudwegTest
         {
             // ARRANGE
             e1 = new Employee("Henrik", "henrik@hotmail.com", false);
+            e2 = new Employee("YesMan", "yes@hotmail.com", false);
             er1 = new EmployeeRepository();
         }
 
@@ -42,6 +44,22 @@ namespace BudwegTest
 
             //ASSERT
             Assert.AreEqual("Henrik", employeeCreate.Name);
+        }
+
+        [TestMethod]
+        public void TestDelete()
+        {
+            //ACT
+            er1.Create(e2);
+
+            //ACT
+            er1.Delete(e2);
+
+            //ACT
+            Employee employee = er1.GetEmployeeId(e2.Email);
+
+            //ASSERT
+            Assert.IsNull(employee);
         }
     }
 }
